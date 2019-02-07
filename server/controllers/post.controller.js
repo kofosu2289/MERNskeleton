e0db647b5ee0989a0b2112d92243f815c9ce5e80
Kenneth Ofosu <kennethofosu01@gmail.com>
2019-02-07 15:46:15 -0500

    403 response for authorization fail

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -149,7 +149,7 @@ const comment = (req, res) => {
   const isPoster = (req, res, next) => {
     let isPoster = req.post && req.auth && req.post.postedBy._id == req.auth._id
     if(!isPoster){
-      return res.status('401').json({
+      return res.status('403').json({
         error: "User is not authorized"
       })
     }
